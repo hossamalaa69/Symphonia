2405d40d0d163314dc709801b018dc149451b911
isemo69 <hossamalaa69.ha@gmail.com>
2020-03-15 14:00:11 +0200

    Add animations to start page

--- app/src/main/java/com/example/symphonia/Activities/UserUI/StartActivity.java
+++ app/src/main/java/com/example/symphonia/Activities/UserUI/StartActivity.java
@@ -1,10 +1,13 @@
 package com.example.symphonia.Activities.UserUI;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.symphonia.Activities.ArtistRegister.WelcomeArtistActivity;
 import com.example.symphonia.Activities.ListenerRegister.WelcomeListenerActivity;
@@ -16,6 +19,12 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        RelativeLayout relativeLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(2000);
+        animationDrawable.start();
     }
 
     public void openListener(View view) {
