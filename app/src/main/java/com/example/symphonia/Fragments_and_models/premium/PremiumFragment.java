218036731550ca8a887f687ad6a36bae77f3194c
isemo69 <hossamalaa69.ha@gmail.com>
2020-03-21 02:17:25 +0200

    Premium UI

--- app/src/main/java/com/example/symphonia/Fragments_and_models/premium/PremiumFragment.java
+++ app/src/main/java/com/example/symphonia/Fragments_and_models/premium/PremiumFragment.java
@@ -1,6 +1,7 @@
 package com.example.symphonia.Fragments_and_models.premium;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,18 +19,16 @@ public class PremiumFragment extends Fragment {
 
     private PremiumViewModel premiumViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        premiumViewModel =
-                ViewModelProviders.of(this).get(PremiumViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        premiumViewModel = ViewModelProviders.of(this).get(PremiumViewModel.class);
         View root = inflater.inflate(R.layout.fragment_premium, container, false);
-        final TextView textView = root.findViewById(R.id.text_premium);
-        premiumViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        TextView t1 = (TextView) root.findViewById(R.id.t1);
+        t1.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
+
         return root;
     }
 }
