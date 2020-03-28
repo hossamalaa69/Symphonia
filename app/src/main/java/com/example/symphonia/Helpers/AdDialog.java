e2d269060bb0b2f06188710ff09e6754e3b035bc
isemo69 <hossamalaa69.ha@gmail.com>
2020-03-28 18:13:14 +0200

    Ad-break animation

--- app/src/main/java/com/example/symphonia/Helpers/AdDialog.java
+++ app/src/main/java/com/example/symphonia/Helpers/AdDialog.java
@@ -7,7 +7,9 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -17,6 +19,12 @@ import com.example.symphonia.Activities.User_Interface.MainActivity;
 import com.example.symphonia.R;
 
 public class AdDialog extends AppCompatActivity {
+
+    private float dX;
+    private float dY;
+
+    private float InitX;
+    private float InitY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +36,49 @@ public class AdDialog extends AppCompatActivity {
         WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
         lp.copyFrom(getWindow().getAttributes());
         lp.width = WindowManager.LayoutParams.MATCH_PARENT;
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.MATCH_PARENT;
         lp.gravity = Gravity.CENTER;
         getWindow().setAttributes(lp);
 
         setFinishOnTouchOutside(false);
+
+        View view = findViewById(R.id.main_view);
+        InitX = view.getX();
+        InitY = view.getY();
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view,MotionEvent event) {
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+
+                        dX = view.getX() - event.getRawX();
+                        dY = view.getY() - event.getRawY();
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+
+                        view.animate()
+                                .x(event.getRawX() + dX)
+                                .y(event.getRawY() + dY)
+                                .setDuration(0)
+                                .start();
+                        Log.d("position now:","("+view.getX()+","+view.getY()+")");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        view.animate()
+                        .translationX(0)
+                        .translationY(0)
+                        .setDuration(200);
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
 
         Button btn_promote = (Button) findViewById(R.id.promote_premium);
         final Button btn_dismiss = (Button) findViewById(R.id.dismiss_ad);
@@ -74,4 +120,5 @@ public class AdDialog extends AppCompatActivity {
 
     @Override
     public void onBackPressed(){ }
+
 }
