bdc1a5fca19966caab734a7736e40127291746d8
isemo69 <hossamalaa69.ha@gmail.com>
2020-03-28 15:16:40 +0200

    Make Ad scrollable for small phones

--- app/src/main/java/com/example/symphonia/Helpers/AdDialog.java
+++ app/src/main/java/com/example/symphonia/Helpers/AdDialog.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.CountDownTimer;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
@@ -67,6 +68,7 @@ public class AdDialog {
 
 
         dialog.show();
+
     }
 
 
